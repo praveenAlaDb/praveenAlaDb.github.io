5b278f93c8d5e0ce6a82e03c3a8adaf5e8d4c1e1
praveenAlaDb <45351129+praveenAlaDb@users.noreply.github.com>
2019-01-04 18:54:07 +0530

    Update Api.py

--- Api.py
+++ Api.py
@@ -3,10 +3,17 @@
 
 
 
-def call():
-    print "yes call called"
-    return "something"
+# def call():
+#     print "yes call called"
+#     return "something"
 
-if __name__ == ' __main__':
-    print "yup"
-    call()
+# if __name__ == ' __main__':
+#     print "yup"
+#     call()
+
+
+from __future__ import print_function
+x = input("Enter a number: ")
+for i in range (1,11,1):
+  print(x, "x", i, "=", (x * i))
+return "something"
